558d895e0b63d74f026d842cc218d3e75810a119
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 16:02:27 -0700

    Implemented getting user input, tokenizing, calling add and subtract.

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+def add(num1, num2):
+    """ Return sum of two intergers"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """ Return subraction of second integer from first"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """ Return product of the inputs"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """ divides the first input by the second, returning a floating number"""
+    return num1 / float(num2)
+
+def square(num1):
+    """ Return square of the interger"""
+    return num1 * num1
+
+def cube(num1):
+    """ Return cube of the interger"""
+    return num1 ** 3
+
+def power(num1, num2):
+    """ Returns the result of raising the first input to the power of the second input"""
+    return num1 ** num2
+
+def mod(num1, num2):
+    """ Returns the remainder when the first input is divided by the second input
+"""
+    return num1 % num2
---
+++ calculator.py
@@ -0,0 +1,21 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+while True:
+	user_input = raw_input("Enter Math Function Here: ")
+	tokens = user_input.split(" ")
+	if tokens[0] == 'q':
+		break
+	elif tokens[0] == '+':
+		result = add(int(tokens[1]), int(tokens[2]))
+		print result
+	elif tokens[0] == '-':
+		result = subtract(int(tokens[1]), int(tokens[2]))
+		print result
